5ea063cc0ca9fed9a6f560773d3210f9a79d4cdf
victor <maiavictorh@gmail.com>
2025-08-29 18:33:28 +0100

    memcpy and memmove

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: victode- <victode-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 01:52:29 by victode-          #+#    #+#             */
-/*   Updated: 2025/08/28 02:01:21 by victode-         ###   ########.fr       */
+/*   Updated: 2025/08/29 14:39:57 by victode-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-	char	*src_byte;
-	char	*dst_byte;
+	size_t		i;
+	const char	*src_byte;
+	char		*dst_byte;
 
 	i = 0;
-	src_byte = (char *)src;
+	src_byte = (const char *)src;
 	dst_byte = (char *)dest;
 	if (!dst_byte && !src_byte)
-		return (0);
+		return (NULL);
 	while (i < n)
 	{
 		dst_byte[i] = src_byte[i];
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,12 +6,30 @@
 /*   By: victode- <victode-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 02:19:51 by victode-          #+#    #+#             */
-/*   Updated: 2025/08/28 02:25:52 by victode-         ###   ########.fr       */
+/*   Updated: 2025/08/29 14:42:07 by victode-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void	*ft_memmove(void *dest, const void *src, size_t n)
-// {
-// }
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t		i;
+	const char	*src_byte;
+	char		*dst_byte;
+
+	i = 0;
+	src_byte = (const char *)src;
+	dst_byte = (char *)dest;
+	if (!dest || !src)
+		return (NULL);
+	if (src < dest)
+	{
+		i = n;
+		while (i--)
+			dst_byte[i] = src_byte[i];
+	}
+	else
+		ft_memcpy(dest, src, n);
+	return (dest);
+}
