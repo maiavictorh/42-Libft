81f2a4005f0e365d1289b222b3f7aaf9c73b7177
victode- <maiavictorh@gmail.com>
2025-09-07 00:06:59 +0100

    output functions and strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,24 +6,35 @@
 /*   By: victode- <victode-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 18:26:16 by victode-          #+#    #+#             */
-/*   Updated: 2025/09/01 19:15:58 by victode-         ###   ########.fr       */
+/*   Updated: 2025/09/06 23:00:21 by victode-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-static int	is_set(char const c, char const *set)
-{
-	while (*set)
-	{
-		if (c == *set)
-			return (1);
-		set++;
-	}
-	return (0);
-}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	i;
+	size_t	start;
+	size_t	end;
+	char	*trim;
+
+	if (!s1)
+		return (NULL);
+	if (s1[0] == 0)
+		return (ft_strdup(""));
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	trim = (char *)malloc(end - start + 2);
+	if (!trim)
+		return (NULL);
+	i = 0;
+	while (start <= end)
+		trim[i++] = s1[start++];
+	trim[i] = 0;
+	return (trim);
 }
-*/
